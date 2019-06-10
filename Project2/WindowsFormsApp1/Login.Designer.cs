93144209f7edccc444df6bb7eca2b034687a3f3e
Redon Osmanollaj <redon_osmanollaj@hotmail.com>
2019-06-11 00:39:38 +0200

    ...

--- Project2/WindowsFormsApp1/Login.Designer.cs
+++ Project2/WindowsFormsApp1/Login.Designer.cs
@@ -42,51 +42,58 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(177, 37);
+            this.textBox1.Location = new System.Drawing.Point(133, 30);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(244, 22);
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(177, 107);
+            this.textBox2.Location = new System.Drawing.Point(133, 87);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(244, 22);
+            this.textBox2.Size = new System.Drawing.Size(184, 20);
             this.textBox2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(72, 42);
+            this.label1.Location = new System.Drawing.Point(54, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.Size = new System.Drawing.Size(68, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "USERNAME";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(69, 112);
+            this.label2.Location = new System.Drawing.Point(52, 91);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.Size = new System.Drawing.Size(70, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "PASSWORD";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(316, 167);
+            this.button1.Location = new System.Drawing.Point(237, 136);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 42);
+            this.button1.Size = new System.Drawing.Size(79, 34);
             this.button1.TabIndex = 4;
             this.button1.Text = "Log in";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(365, 212);
+            this.linkLabel1.Location = new System.Drawing.Point(274, 172);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(56, 17);
+            this.linkLabel1.Size = new System.Drawing.Size(43, 13);
             this.linkLabel1.TabIndex = 5;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Sign up";
@@ -99,9 +106,11 @@
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(343, 48);
+            this.groupBox1.Location = new System.Drawing.Point(257, 39);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(435, 263);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(326, 214);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
@@ -109,22 +118,24 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.login;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 48);
+            this.pictureBox1.Location = new System.Drawing.Point(11, 39);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(303, 304);
+            this.pictureBox1.Size = new System.Drawing.Size(227, 247);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Login";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
--- Project2/WindowsFormsApp1/Login.cs
+++ Project2/WindowsFormsApp1/Login.cs
@@ -7,11 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
 
 namespace WindowsFormsApp1
 {
     public partial class Login : Form
     {
+        public static RSACryptoServiceProvider objRsa;
+        public static DESCryptoServiceProvider objDes;
+        public static Socket clientSocket;
+
         public Login()
         {
             InitializeComponent();
@@ -19,7 +27,65 @@ namespace WindowsFormsApp1
 
         private void Login_Load(object sender, EventArgs e)
         {
+            createRsa();
+            createDes();
+            createClientSocket();
+            string serverIp = "127.0.0.1";
+            int port = 12000;
 
+            try
+            {
+                clientSocket.Connect(new IPEndPoint(IPAddress.Parse(serverIp), port));
+            }
+            catch(SocketException ex)
+            {
+                MessageBox.Show("Couldn't connect to this server!\n"+ex.Message);
+            }
+
+            clientSocket.Send(Encoding.UTF8.GetBytes(enkriptoCelesin()));
+
+        }
+
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void createClientSocket()
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        }
+
+        private static void createDes()
+        {
+            objDes = new DESCryptoServiceProvider();
+            objDes.GenerateIV();
+            objDes.Padding = PaddingMode.Zeros;
+            objDes.Mode = CipherMode.CBC;
+            objDes.GenerateKey();
+        }
+
+        private void createRsa()
+        {
+            objRsa = new RSACryptoServiceProvider();
+            string path = "C:\\Users\\Admin\\Documents\\GitHub\\DS_1819_Gr15\\Project2\\Server\\bin\\Debug\\ServerPublicKey.xml";
+            StreamReader sr = new StreamReader(path);
+            string strXmlParametrat = sr.ReadToEnd();
+            sr.Close();
+
+
+            objRsa.FromXmlString(strXmlParametrat);
+        }
+
+        private string enkriptoCelesin()
+        {
+            byte[] byteCelesiPlain = objDes.Key;
+            byte[] byteCelesiCipher = objRsa.Encrypt(byteCelesiPlain, true);
+
+            return Convert.ToBase64String(byteCelesiCipher);
         }
     }
 }
