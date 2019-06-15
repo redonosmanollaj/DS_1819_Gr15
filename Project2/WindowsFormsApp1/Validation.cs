5547ab30d45f7b9e0c14e7357f54470927724210
lirimimeri <lirimimeri17@gmail.com>
2019-06-15 13:46:41 +0200

    Update Validation.cs

--- Project2/WindowsFormsApp1/Validation.cs
+++ Project2/WindowsFormsApp1/Validation.cs
@@ -9,14 +9,37 @@ namespace WindowsFormsApp1
 {
     class Validation
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        
 
         public Validation()
         {
 
         }
-        public bool validateName(string input)
+        public static bool validateName(string input)
+        {
+            string pattern = "[^a-zA-Z]";
+            if (Regex.IsMatch(input, pattern) && input.Length > 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool validateSurname(string input)
+        {
+            string pattern = "[^a-zA-Z]";
+            if (Regex.IsMatch(input, pattern) && input.Length > 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool validateDegree(string input)
         {
             string pattern = "[^a-zA-Z]";
             if (Regex.IsMatch(input, pattern))
@@ -28,33 +51,9 @@ namespace WindowsFormsApp1
                 return false;
             }
         }
-        public bool validateSurname(string input)
+        public static bool validateSalary(double input)
         {
-            string pattern = "[^a-zA-Z]";
-            if (Regex.IsMatch(input, pattern))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public bool validateDegree(string input)
-        {
-            string pattern = "[^a-zA-Z]";
-            if (Regex.IsMatch(input, pattern))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public bool validateSalary(double input)
-        {
-            if (input == double.NaN)
+            if (input == double.NaN && input < 150 && input > 2000)
             {
                 return false;
             }
@@ -63,10 +62,10 @@ namespace WindowsFormsApp1
                 return true;
             }
         }
-        public bool validateUsername(string input)
+        public static bool validateUsername(string input)
         {
             string pattern = "[^a-zA-Z0-9]";
-            if (Regex.IsMatch(input, pattern))
+            if (Regex.IsMatch(input, pattern) && input.Length > 5)
             {
                 return true;
             }
@@ -75,7 +74,7 @@ namespace WindowsFormsApp1
                 return false;
             }
         }
-        public bool validatePass(string input)
+        public static bool validatePass(string input)
         {
             if (input.Length > 6)
             {
@@ -86,7 +85,7 @@ namespace WindowsFormsApp1
                 return false;
             }
         }
-        public bool validateEmail(string input)
+        public static bool validateEmail(string input)
         {
             string pattern = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
             if (Regex.IsMatch(input, pattern,RegexOptions.IgnoreCase))
@@ -97,10 +96,9 @@ namespace WindowsFormsApp1
             {
                 return false;
             }
+            
+            
         }
-        
-
-
-
+ 
     }
 }
